ec5420a14373b3603c74cd01279e114e87cbff77
Yadhu-Ayyanchira <123824804+Yadhu-Ayyanchira@users.noreply.github.com>
2023-06-23 09:22:42 +0530

    23/6/2023

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ app.use("/admin", adminRoute);
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err);
-  //res.status(500).send('Internal Server Error')
   res.render('404')
 });
 
